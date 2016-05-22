4a8473a3d4790c4f4c168c2ab35779c21e9b34c5
Peter Carbonetto <pcarbo@uchicago.edu>
2016-05-21 21:12:39 -0700

    calc.ld.cfw.R is working.

--- R/calc.ld.cfw.R
+++ R/calc.ld.cfw.R
@@ -1,19 +1,16 @@
-# This is Peter's script for estimating LD decay in the CFW mice.
-#
-# These calculations use the genotype data for 1,150 male CFW mice at
-# 92,734 SNPs on autosomal chromosomes. The base-pair positions are
-# based on NCBI release 38 of the mouse genome assembly.
-#
-# We use the --r2 command in PLINK v1.9beta3.33 to compute the r^2
-# measure of LD (see Pritchard and Przeworski, 2001) based on
-# maximum-likelihood estimates of the haplotype frequences.
-#
+# Script for estimating LD decay in the CFW mice. These calculations
+# use the genotype data for 1,150 male CFW mice at 92,734 SNPs on
+# autosomal chromosomes. The base-pair positions are based on NCBI
+# release 38 of the mouse genome assembly. The --r2 command in PLINK
+# v1.9beta3.33 is used to compute the r^2 measure of LD (see Pritchard
+# and Przeworski, 2001) based on maximum-likelihood estimates of the
+# haplotype frequencies.
 library(data.table)
 
 # SCRIPT PARAMETERS
 # -----------------
-# PLINK file containing the genotype data.
-geno.file <- "/tmp/pcarbo/data/cfw/cfw"
+# PLINK .bed/.bim/.fam files containing the genotype data.
+geno.file <- "cfw"
 
 # These are the bins used for the LD calculations.
 bins <- seq(0,5e6,1e5)
@@ -27,16 +24,16 @@ set.seed(1)
 # LOAD SNP INFO
 # -------------
 cat("Loading SNP data.\n")
-map <- read.table("/tmp/pcarbo/data/cfw/cfw.bim",stringsAsFactors = FALSE)
+map <- read.table("cfw.bim",stringsAsFactors = FALSE)
 names(map) <- c("chr","id","dist","pos","A1","A2")
 cat("Loaded SNP data for",nrow(map),"markers.\n")
 
 # LOAD SNP ALLELE FREQUENCIES
 # ---------------------------
 cat("Loading SNP allele frequencies")
-system(sprintf("~/bin/plink2 --bfile %s --freq --out /tmp/pcarbo/plink/cfw",
-               geno.file))
-out <- read.table("/tmp/pcarbo/plink/cfw.frq",header = TRUE,
+system("mkdir -p out_plink")
+system(sprintf("plink2 --bfile %s --freq --out out_plink/cfw",geno.file))
+out <- read.table("out_plink/cfw.frq",header = TRUE,
                   stringsAsFactors = FALSE)
 map <- cbind(map,data.frame(maf = out$MAF))
 rm(out)
@@ -49,8 +46,8 @@ caterase <- function (s)
   cat(s,rep("\b",nchar(s)),sep = "")
 for (i in 1:19) {
   caterase(sprintf("chromosome %d",i))
-  out.file <- sprintf("/tmp/pcarbo/plink/chr%d",i)
-  system(sprintf("~/bin/plink2 --bfile %s --chr %d --r2 square --out %s",
+  out.file <- sprintf("out_plink/chr%d",i)
+  system(sprintf("plink2 --bfile %s --chr %d --r2 square --out %s",
                  geno.file,i,out.file),ignore.stdout = TRUE)
   r <- fread(paste0(out.file,".ld"),verbose = FALSE,showProgress = FALSE)
   class(r)    <- "data.frame"
